ef8841c5f2d6240a6cb4780a87ddcd193cec09c6
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-10-19 13:49:36 +0530

    Add files via upload

---
+++ pr8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter name: "<<endl;
+    cin>>name;
+    int age;
+    cout<<"Enter age: "<<endl;
+    cin>>age;
+    float height;
+    cout<<"Enter your height: "<<endl;
+    cin>>height;
+    double pi;
+    cout<<"Enter 4 digits of pi: "<<endl;
+    cin>>pi;
+    bool married;
+    cout<<"Married: "<<endl;
+    cin>>married;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Age: "<<age<<endl;
+    cout<<"Height: "<<height<<endl;
+    cout<<"PI: "<<pi<<endl;
+    cout<<"Married: "<<married<<endl;
+
+
+}
